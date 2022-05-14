64a21c894a7766dec9a619728004b458ed08c2d1
ahhhh6980 <ahhhh6980@gmail.com>
2022-05-13 20:49:35 -0400

    Added a prelude file for quick import of what I believe to be the most used features

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 #![allow(incomplete_features)]
+#![feature(const_trait_impl)]
 #![feature(adt_const_params)]
 
 pub mod colors;
+pub mod prelude;
 pub mod types;
 pub use colors::*;
 pub use types::*;
